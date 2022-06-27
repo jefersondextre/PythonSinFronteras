14274b0eceeeb0bcb693b93b3021a57601cafedf
jadex_Desktop <jadexb04@gmail.com>
2022-06-26 21:29:09 -0500

    Seccion 4 y 5Introduccion a datos y tipos de datos

---
+++ 4.-Introduccion.py
@@ -0,0 +1,46 @@
+# Comentario de una linea
+#? Esto es un comentario en Python
+'''
+Esto tambien es un comentario
+de varias lineas
+para colocarlo
+en bloque
+'''
+from ast import operator
+
+
+if 5 > 3:
+  print('5 es mayor que 3')
+
+if 3<5:
+  print('3 no es mayor que 5')
+
+# VAriables:
+x = 5
+y='2daVariable'
+print(x,y)
+# Los Nombres de las variables deben ser mas expresivos, para saber que hacen
+email = 'jadexb04@gmail.com'
+print(email)
+
+_mi_var = 'chanchito'
+miVar = 'Camelcase'
+MIVAR= 'variable'
+mivar34 = 'Variable con numero'
+
+
+# VARIABLES MULTIPLES
+a,b,c='Lala','Lele','Lili'
+print(a,b,c)
+
+variable = A = B = C = 'otro'
+print(variable,A,C)
+
+# Concatenacion
+inicio = 'Hola '
+final= 'mundo'
+print(inicio + final)
+
+
+
+
---
+++ 5.TiposDeDatos.py
@@ -0,0 +1,12 @@
+# Strings y numeros
+# ======================
+# String
+palabra = 'hola mundo'
+oracion = "hola mundo comilla doble"
+# entero
+entero = 25
+# float
+decimales = 20.5
+# complejos
+complejo= 1j
+print(palabra, oracion  ,entero,decimales,complejo)
