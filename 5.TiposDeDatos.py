535509235516c4885b3d53a44a6b5e3f9244f987
jadex_Desktop <jadexb04@gmail.com>
2022-06-26 23:04:40 -0500

    video 20 contando listas

--- 5.TiposDeDatos.py
+++ 5.TiposDeDatos.py
@@ -9,4 +9,26 @@ entero = 25
 decimales = 20.5
 # complejos
 complejo= 1j
-print(palabra, oracion  ,entero,decimales,complejo)
+# print(palabra, oracion  ,entero,decimales,complejo)
+
+# Listas
+lista1 = []
+lista = [1,2,3]
+print(lista)
+
+lista.append(4)
+print(lista)
+
+lista2=lista.copy()
+print('lista2 '+str(lista2))
+
+print(lista2.count(4))
+print(len(lista))
+largoLista=len(lista)
+largoLista2=len(lista2)
+print(largoLista,largoLista2)
+
+# print(lista2)
+
+# lista.clear()
+# print(lista)
