e8687c2adc869b9d9d34f8985c2c7f53dda41ccb
jadex_Desktop <jadexb04@gmail.com>
2022-06-29 16:05:07 -0500

    video de condicionales if else

---
+++ 6.-ControlDeFlujo.py
@@ -0,0 +1,32 @@
+if 2 <= 5:
+  print("2 es menor que 5")
+  
+#  a == b
+#  a < b
+#  a > b
+#  a != b
+#  a <= b
+#  a >= b
+
+if 2 == 2:
+  print("2 es igual a 2")
+
+if 2 == 3:
+  print("2 es igual a 3")
+
+if 2 > 5:
+  print("2 es mayor a 5")
+
+if 5>2:
+  print("5 es mayor a 2")
+  
+if 2 != 2:
+  print("2 es distinto a 2")
+  
+  
+if 2>5:
+  print('lala')
+elif 5<5:
+  print('2 menor a 5 en elif')
+else:
+  print('yo me imprimo solo si todo lo anterior evalua en falso')  
