cfae17aea94696413746c5469f7a180e00c10b23
jadex_Desktop <jadexb04@gmail.com>
2022-07-13 00:09:30 -0500

    8 Lopps video 41

--- 4.-Introduccion.py
+++ 4.-Introduccion.py
@@ -1,5 +1,5 @@
 # Comentario de una linea
-#? Esto es un comentario en Python
+# ? Esto es un comentario en Python
 '''
 Esto tambien es un comentario
 de varias lineas
@@ -10,37 +10,33 @@ from ast import operator
 
 
 if 5 > 3:
-  print('5 es mayor que 3')
+    print('5 es mayor que 3')
 
-if 3<5:
-  print('3 no es mayor que 5')
+if 3 < 5:
+    print('3 no es mayor que 5')
 
 # VAriables:
 x = 5
-y='2daVariable'
-print(x,y)
+y = '2daVariable'
+print(x, y)
 # Los Nombres de las variables deben ser mas expresivos, para saber que hacen
 email = 'jadexb04@gmail.com'
 print(email)
 
 _mi_var = 'chanchito'
 miVar = 'Camelcase'
-MIVAR= 'variable'
+MIVAR = 'variable'
 mivar34 = 'Variable con numero'
 
 
 # VARIABLES MULTIPLES
-a,b,c='Lala','Lele','Lili'
-print(a,b,c)
+a, b, c = 'Lala', 'Lele', 'Lili'
+print(a, b, c)
 
 variable = A = B = C = 'otro'
-print(variable,A,C)
+print(variable, A, C)
 
 # Concatenacion
 inicio = 'Hola '
-final= 'mundo'
+final = 'mundo'
 print(inicio + final)
-
-
-
-
--- 5.3TiposDeDatos.py
+++ 5.3TiposDeDatos.py
@@ -1,38 +1,38 @@
 # Diccionarios anidados
 gatitos1 = {
-  "fluffy":{
-      "nombre":"Fluffy",
-      "edad": 4,
-  },
-  "Mamba":{
-    "nombre":"Black Mamba",
-    "edad":12
-  }
+    "fluffy": {
+        "nombre": "Fluffy",
+        "edad": 4,
+    },
+    "Mamba": {
+        "nombre": "Black Mamba",
+        "edad": 12
+    }
 }
 print(gatitos1)
 
 # =================================== #
 Fluffy = {
-    "nombre":"Fluffy",
+    "nombre": "Fluffy",
     "edad": 4,
 }
 Mamba = {
-    "nombre":"Black Mamba",
-    "edad":12
-  }
+    "nombre": "Black Mamba",
+    "edad": 12
+}
 
-gatitos2 ={
-  "Fluffy": Fluffy,
-  "Mamba": Mamba
+gatitos2 = {
+    "Fluffy": Fluffy,
+    "Mamba": Mamba
 }
 print(gatitos2)
 
 # ==============================
-perritos = dict(nombre="Chanchito Feliz",edad=6)
+perritos = dict(nombre="Chanchito Feliz", edad=6)
 print(perritos)
 
 
 # Booleanos: True o False
 verdadero = True
-falso= False
-print(verdadero,falso)
+falso = False
+print(verdadero, falso)
