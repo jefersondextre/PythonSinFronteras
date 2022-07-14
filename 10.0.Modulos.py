6978a97958214d5c2a79a7626b76fc87aadb0a92
estadisticaDESI_ <estadisticainternacional2021@gmail.com>
2022-07-14 12:18:53 -0500

    importacion de archivos

---
+++ 10.0.Modulos.py
@@ -0,0 +1,14 @@
+import modulos as xs
+# Si no necesitamos todo el modulo, solo necesitamos un  metodo o algo del archivo usamos
+
+# from modulos import saludo,otroMetodo, otracosa
+from modulos import saludo
+
+# print(xs.mascotas)
+
+# xs.saludo('Nicolas')
+
+saludo('Jeferson')
+
+# PIP es un administrador de paquetes instalados en Python, permitiendonos instalar modulos, listar , agregar, eliminar modulos
+
---
+++ modulos.py
@@ -0,0 +1,5 @@
+mascotas = ['chanchito feliz', 'Felipe', 'Flufy', 'Hashicko']
+
+
+def saludo(nombre):
+  print('Hola! ', nombre)
