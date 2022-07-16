7c713bbf11362d2730f1b14a4e5d5d780d52529f
jadex_Desktop <jadexb04@gmail.com>
2022-07-16 00:23:15 -0500

    Ejercicio 1 sobre multiplicacion de dos numeros sin usar el simbolo x

---
+++ 12.0_Ejercicios.py
@@ -0,0 +1,21 @@
+# multiplicar dos números sin usar el símbolo de multiplicación
+a = 4
+b = 8
+resultado = 0
+for x in range(a):
+  resultado +=b
+
+print(resultado)
+# ingresar nombre y apellido e imprimirlo al reves
+
+
+# escribir una función que encuentre el elemento menor de una lista
+# escribir una función que devuelva el volumen de una esfera por su radio
+# 4/3 * pi * r ** 3
+# escribir una función que indique si el usuario es mayor de edad
+# escribir una función que indique si un número es par o impar
+# escribir una función que indique cuantas vocales tiene una palabra
+# escribir una aplicación que reciba una cantidad infinita de números hasta
+# decir basta, luego que devuelva la suma de los números ingresados
+# escribir una función que reciba nombre y apellido y los vaya agregando a
+# un archivo
